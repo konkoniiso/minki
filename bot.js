46a9ee33be3a9e135e86173886d17c2c48e9c433
konkoniiso <66762989+konkoniiso@users.noreply.github.com>
2020-07-25 05:58:46 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
+client.on('ready', function () {
+    console.log('${bot.user.username} Is Online!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
-});
-
-// THIS  MUST  BE  THIS  WAY
-client.login(NzE3NDQ2NzkyODExMTE4NzUz.XxlOIw.7LGMbA3XWJ-vxJ4A1_2u9hJpkSQ);
+client.login(process.env.token);

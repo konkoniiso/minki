afd9da31aac786cc0afc1a858188ad96c121f41c
konkoniiso <66762989+konkoniiso@users.noreply.github.com>
2020-07-25 05:34:59 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,35 +1,15 @@
-{
-  "ClientId": 717446792811118753,
-  "Token": "NzE3NDQ2NzkyODExMTE4NzUz.XxlOIw.7LGMbA3XWJ-vxJ4A1_2u9hJpkSQ",
-  "OwnerIds": [
-    170741085545562114
-  ],
-  "LoLApiKey": "",
-  "GoogleApiKey": "",
-  "MashapeKey": "",
-  "OsuApiKey": "",
-  "SoundCloudClientId": "",
-  "CleverbotApiKey": "",
-  "CarbonKey": "",
-  "Db": {
-    "Type": "sqlite",
-    "ConnectionString": "Data Source=data/NadekoBot.db"
-  },
-  "TotalShards": 1,
-  "PatreonAccessToken": "",
-  "PatreonCampaignId": "334038",
-  "RestartCommand": null,
-  "ShardRunCommand": "",
-  "ShardRunArguments": "",
-  "ShardRunPort": null,
-  "MiningProxyUrl": null,
-  "MiningProxyCreds": null,
-  "BotListToken": null,
-  "TwitchClientId": null,
-  "VotesToken": null,
-  "VotesUrl": null,
-  "RedisOptions": null,
-  "LocationIqApiKey": null,
-  "TimezoneDbApiKey": null,
-  "CoinmarketcapApiKey": null
-}
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);

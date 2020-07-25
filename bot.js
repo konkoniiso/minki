31b41cecd0dc3cb746d8375d9b50ba817e640c95
konkoniiso <66762989+konkoniiso@users.noreply.github.com>
2020-07-25 06:01:33 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const bot = new Discord.Client();
 
-client.on('ready', function () {
+bot.on('ready', function () {
     console.log('${bot.user.username} Is Online!');
 });
 
-client.login(process.env.token);
+bot.login(process.env.token);
